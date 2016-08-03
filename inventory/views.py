937fd18ea330f5066014bf9b82995d47206a9e8c
Caleb Carvalho <you@example.com>
2016-08-03 08:19:34 +0200

    screen pix

--- inventory/views.py
+++ inventory/views.py
@@ -20,3 +20,12 @@ def item_details(request, item_id):
         'item': item
     })
 
+
+def full_image(request, item_id):
+    try:
+        item = Item.objects.get(id=item_id)
+    except Item.DoesNotExist:
+        raise Http404('This item does not exist')
+    return render(request, 'inventory/full_image.html', {
+        'item': item
+    })
--- myfirstdjango/urls.py
+++ myfirstdjango/urls.py
@@ -6,7 +6,8 @@ from inventory import views
 from myfirstdjango import settings
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^item/(?P<item_id>\d+)/', views.item_details, name='item_detail'),
-    url(r'^admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  url(r'^$', views.index, name='index'),
+                  url(r'^item/(?P<item_id>\d+)/', views.item_details, name='item_detail'),
+                  url(r'^item/(?P<id>\d+)/', views.full_image, name='full_image'),
+                  url(r'^admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
