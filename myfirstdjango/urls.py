8f1da6b4faf265cc86eb6145586414d9257ffcc2
Caleb Carvalho <you@example.com>
2016-08-11 06:54:52 +0200

    fix static url

--- myfirstdjango/urls.py
+++ myfirstdjango/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.static import serve
 
 from inventory import views
 from myfirstdjango import settings
@@ -9,6 +10,5 @@ urlpatterns = [
                   url(r'^$', views.index, name='index'),
                   url(r'^item/(?P<item_id>\d+)/', views.item_details, name='item_detail'),
                   url(r'^admin/', admin.site.urls),
-                  url(r'^static/(?P<path>.*)$',
-                      'django.views.static.serve')
+                  url(r'^static/(?P<path>.*)$', serve)
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
