5fd8947ca11707012540f9e223ae20ddbe66bc13
Caleb Carvalho <you@example.com>
2016-10-04 22:18:43 +0200

    fixing likes

--- myfirstdjango/static/js/my_javascript.js
+++ myfirstdjango/static/js/my_javascript.js
@@ -1,4 +1,6 @@
 // using jQuery
+var okResponse=null, loginName=null, loginPicture=null;
+
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -13,4 +15,42 @@ function getCookie(name) {
         }
     }
     return cookieValue;
+}
+
+function getUserAuthStatus(response) {
+    okResponse = response.status;
+    return response.status;
+}
+
+function getChangedStatus(){
+
+    return okResponse;
+}
+function getUserInfo(){
+      FB.api(
+      '/me',
+      'GET',
+      {"fields":"id,name"},
+      function(response) {
+          loginName=response.name;  
+      });
+    };
+
+function getUserPicture(){
+      FB.api("/me/picture",
+      {"type":"small"},
+      function(response) {
+        if (response && !response.error) {
+          loginPicture=response.data.url;
+        }
+         
+      });
+    };
+
+function getUserName(){
+    return loginName;
+}    
+
+function getPicture(){
+    return loginPicture;
 }
