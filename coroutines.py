e5039d0810f43d03c0750e867082a5d54cb82e71
Caleb Carvalho <Havanero@users.noreply.github.com>
2017-04-12 15:24:47 +0100

    Update coroutines.py

--- coroutines.py
+++ coroutines.py
@@ -1,2 +1,30 @@
-def coroutine(func):    def start(*args,**kwargs):        cr = func(*args,**kwargs)        cr.__next__()        return cr    return start
-class COR(object):   def __init__(self, pattern):       self.count=0       self.pattern=pattern       self.find=self._grep()   @coroutine   def _grep(self):        while True:           line=yield           if line in self.pattern:                print(self.count, line)               self.count+=1   def send(self, arg):       self.find.send(arg)   def close(self):        print("Closing Courtine after finding {} total count".format(self.count))       self.find.close()   def next(self):       self.find.__next__()
+def coroutine(func):    
+  def start(*args,**kwargs):       
+    cr = func(*args,**kwargs)        
+    cr.__next__()        
+    return cr    
+  return start
+
+class COR(object):  
+  def __init__(self, pattern):       
+    self.count=0       
+    self.pattern=pattern       
+    self.find=self._grep()  
+    
+    @coroutine   
+    def _grep(self):        
+      while True:           
+        line=yield           
+        if line in self.pattern:                
+          print(self.count, line)               
+          self.count+=1   
+      
+      def send(self, arg):       
+            self.find.send(arg)   
+      
+      def close(self):        
+           print("Closing Courtine after finding {} total count".format(self.count))       
+          self.find.close()   
+      
+      def next(self):       
+        self.find.__next__()
